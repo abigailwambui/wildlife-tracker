2785effff0a3c5e405731197b11d07d999c98530
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-03 18:00:58 +0300

    initialize animal with age and health

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,17 +1,31 @@
 import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Animal {
     private String name;
+    private String health;
+    private String age;
     private int sightingId;
     private int id;
 
-  public Animal(String name, int sightingId) {
+  public Animal(String name, String health, String age, int sightingId) {
       this.name = name;
+      this.health = health;
+      this.age = age;
       this.sightingId = sightingId;
   }
 
   public String getName(){
       return name;
+  }
+
+  public String getHealth(){
+      return health;
+  }
+
+  public String getAge(){
+      return age;
   }
 
   public int getSightingId(){
@@ -30,18 +44,29 @@ public class Animal {
     } else {
       Animal newAnimal = (Animal) otherAnimal;
       return this.getName().equals(newAnimal.getName()) &&
+             this.getHealth().equals(newAnimal.getHealth()) &&
+             this.getAge().equals(newAnimal.getAge()) &&
              this.getSightingId() == newAnimal.getSightingId();
     }
   }
 
   public void save() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO animals (name, sightingid) VALUES (:name, :sightingId)";
+      String sql = "INSERT INTO animals (name, health, age, sightingid) VALUES (:name, :health, :age, :sightingId)";
       this.id = (int) con.createQuery(sql, true)
         .addParameter("name", this.name)
+        .addParameter("health", this.health)
+        .addParameter("age", this.age)
         .addParameter("sightingId", this.sightingId)
         .executeUpdate()
         .getKey();
+    }
+  }
+
+  public static List<Animal> all() {
+    String sql = "SELECT * FROM animals";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Animal.class);
     }
   }
 
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -9,41 +9,62 @@ public class AnimalTest {
 
     @Test
     public void animal_instantiatesCorrectly_true() {
-        Animal testAnimal = new Animal("Elephants", 1);
+        Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
         assertEquals(true, testAnimal instanceof Animal);
   }
 
     @Test
     public void animal_instantiatesWithName_String() {
-        Animal testAnimal = new Animal("Elephants", 1);
+        Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
         assertEquals("Elephants", testAnimal.getName());
   }
 
     @Test
-    public void animal_instantiatesWithSightingId_int() {
-        Animal testAnimal = new Animal("Elephants", 1);
-        assertEquals(1, testAnimal.getSightingId());
+    public void animal_instantiatesWithHealth_String() {
+        Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
+        assertEquals("ill", testAnimal.getHealth());
+  }
+
+     @Test
+    public void animal_instantiatesWithAge_String() {
+        Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
+        assertEquals("young", testAnimal.getAge());
   }
 
     @Test
+    public void animal_instantiatesWithSightingId_int() {
+        Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
+        assertEquals(1, testAnimal.getSightingId());
+  }
+    @Test
     public void equals_returnsTrueIfNameAndSightingIdAreSame_true() {
-        Animal testAnimal = new Animal("Elephants", 1);
-        Animal anotherAnimal = new Animal("Elephants", 1);
+        Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
+        Animal anotherAnimal = new Animal("Elephants", "ill", "young", 1);
         assertTrue(testAnimal.equals(anotherAnimal));
   }
 
     @Test
     public void save_returnsTrueIfDescriptionsAretheSame() {
-        Animal testAnimal = new Animal("Elephants", 1);
+        Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
         testAnimal.save();
         assertTrue(Animal.all().get(0).equals(testAnimal));
   }
 
     @Test
     public void save_assignsIdToAnimal() {
-      Animal testAnimal = new Animal("Elephants", 1);
+      Animal testAnimal = new Animal("Elephants", "ill", "young", 1);
       testAnimal.save();
       Animal savedAnimal = Animal.all().get(0);
       assertEquals(savedAnimal.getId(), testAnimal.getId());
+  }
+
+    @Test
+    public void all_returnsAllInstancesOfAnimal_true() {
+      Animal firstAnimal = new Animal("Elephants", "ill", "young", 1);
+      firstAnimal.save();
+      Animal secondAnimal = new Animal("Zebras", "okay", "old", 1);
+      secondAnimal.save();
+      assertEquals(true, Animal.all().get(0).equals(firstAnimal));
+      assertEquals(true, Animal.all().get(1).equals(secondAnimal));
   }
 }
