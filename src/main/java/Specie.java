45dafc2dcc2eb910ff60fe2aa85232080714d379
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-03 18:39:00 +0300

    initialize class specie with name

--- src/main/java/Specie.java
+++ src/main/java/Specie.java
@@ -1,6 +1,12 @@
 public class Specie {
+    private String name;
 
   public Specie(String name, int population, boolean endangered, int sightingId) {
+      this.name = name;
+  }
+
+  public String getName(){
+    return name;
   }
 
 }
--- src/test/java/SpecieTest.java
+++ src/test/java/SpecieTest.java
@@ -2,15 +2,21 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
 
-public class SpeciesTest {
+public class SpecieTest {
 
     @Rule
   public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void Specie_instantiatesCorrectly_true() {
-        Specie testSpecie = new Specie("Bubbles", 1);
+        Specie testSpecie = new Specie("Homo sapiens", 30, false, 1);
         assertEquals(true, testSpecie instanceof Specie);
+  }
+
+    @Test
+    public void Specie_instantiatesWithName_String() {
+      Specie testSpecie = new Specie("Homo sapiens", 30, false, 1);
+      assertEquals("Homo sapiens", testSpecie.getName());
   }
 
 }
